d204dc18d320beb1adb0fcf62a0bf8c877fdd264
Nadir <yusipov.nadir@gmail.com>
2022-06-06 19:29:19 +0300

    изменены функции: main_page(), candidate_id(), skills_id(), так импортирована функция json_load() из utils.py

--- app.py
+++ app.py
@@ -1,42 +1,37 @@
 from flask import Flask
-import json
+from utils import load_json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
-    with open('candidates.json', 'r', encoding="utf-8") as f:
-        candidates_info = json.load(f)
-        result = str()
+    result = str()
 
-        for candidate in candidates_info:
-            result += ('<pre>{}\nИмя кандидата: {}\nПозиция: {}\nНавыки: {}\n<pre>'.format(
-                candidate["picture"], candidate["name"], candidate["position"], candidate["skills"]))
+    for candidate in load_json():
+        result += ('<pre>{}\nИмя кандидата: {}\nПозиция: {}\nНавыки: {}\n<pre>'.format(
+            candidate["picture"], candidate["name"], candidate["position"], candidate["skills"]))
 
-        return result
+    return result
 
 
 @app.route('/candidates/<int:uid>')
 def candidate_id(uid):
-    with open('candidates.json', 'r', encoding="utf-8") as f:
-        candidates_info = json.load(f)
-        for i in candidates_info:
-            if i["id"] == uid:
-                return '<pre>{}\nИмя кандидата: {}\nПозиция: {}\nНавыки: {}\n<pre>'.format(
-                    i["picture"], i["name"], i["position"], i["skills"])
+
+    for i in load_json():
+        if i["id"] == uid:
+            return '<pre>{}\nИмя кандидата: {}\nПозиция: {}\nНавыки: {}\n<pre>'.format(
+                i["picture"], i["name"], i["position"], i["skills"])
 
 
 @app.route('/skills/<x>')
 def skills_id(x):
-    with open('candidates.json', 'r', encoding="utf-8") as f:
-        candidates_info = json.load(f)
-        skill_result = str()
-        for i in candidates_info:
-            if x.lower() in i["skills"]:
-                skill_result += '<pre>{}\nИмя кандидата: {}\nПозиция: {}\nНавыки: {}\n<pre>'.format(
-                    i["picture"], i["name"], i["position"], i["skills"])
-        return skill_result
+    skill_result = str()
+    for i in load_json():
+        if x.lower() in i["skills"].lower():
+            skill_result += '<pre>{}\nИмя кандидата: {}\nПозиция: {}\nНавыки: {}\n<pre>'.format(
+                i["picture"], i["name"], i["position"], i["skills"])
+    return skill_result
 
 
 app.run()
